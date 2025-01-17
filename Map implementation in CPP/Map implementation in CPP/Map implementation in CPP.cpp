4ff9fde2e2956567266650ee773a83b233ca800a
Hamza <hamzakhattak188@gmail.com>
2025-01-17 23:22:40 +0500

    added more stuff to map implementation

--- Map implementation in CPP/Map implementation in CPP/Map implementation in CPP.cpp
+++ Map implementation in CPP/Map implementation in CPP/Map implementation in CPP.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
-
+#include <stdexcept>
 template<typename T1, typename T2>
 struct pair {
     T1 first;
     T2 second;
 
-    // Constructors
+   
     pair(): first(0), second("") {}
     pair(const T1& a, const T2& b) : first(a), second(b) {}
 
-    // Operators
+    
     bool operator==(const pair& other) const {
         return first == other.first && second == other.second;
     }
@@ -19,7 +19,7 @@ struct pair {
     }
 
     bool operator<(const pair& other) const {
-        return (first < other.first) || (first == other.first && second < other.second);
+        return (first < other.first);
     }
 };
 
@@ -45,12 +45,30 @@ class map {
     int n;          // Number of elements
 
 public:
+
+
+
+    //constructor
     map() {
         H = new tNode<K, V>;
         H->left = H;
         H->right = H;
         H->parent = H;
         n = 0;
+    }
+
+
+    //copy-constructor
+
+    map(const map<K, V>& other) {
+        H = new tNode<K, V>;
+        H->left = H;
+        H->right = H;
+        H->parent = H;
+        n = 0;
+        for (auto it = other.begin(); it != other.end(); ++it) {
+            insert(it->first, it->second);
+        }
     }
 
     void insert(const K& key, const V& value) {
@@ -67,10 +85,12 @@ public:
             tNode<K, V>* current = H->parent;
             tNode<K, V>* parent = nullptr;
 
+            //checking to see if the node that we want to insert already exists in the tree or not
+
             while (current != H) {
                 parent = current;
                 if (key == current->data.first) {
-                    current->data.second = value; // Update value if key already exists
+                    current->data.second = value;
                     delete new_node;
                     return;
                 }
@@ -82,6 +102,7 @@ public:
                 }
             }
 
+            //from here i am inserting the node in the tree
             if (key < parent->data.first) {
                 parent->left = new_node;
             }
@@ -93,26 +114,13 @@ public:
         n++;
     }
 
-    iterator find(const K& key) const {
-        tNode<K, V>* current = H->parent;
-        while (current != H) {
-            if (key == current->data.first) {
-                return iterator(current);
-            }
-            else if (key < current->data.first) {
-                current = current->left;
-            }
-            else {
-                current = current->right;
-            }
-        }
-        return iterator(H); // Return an iterator to H, which acts as end()
-    }
 
 
     void erase(const K& key) {
         tNode<K, V>* current = H->parent;
         tNode<K, V>* parent = H;
+
+        //searching for the node that i want to delete and if i find it will break from the loop
 
         while (current != H) {
             if (key == current->data.first) {
@@ -130,8 +138,11 @@ public:
         if (current == H) {
             std::cout << "The key isn't found" << std::endl;
             return;
+            // in this case the node is not found so we simply returned from the statement
         }
 
+        //now that our traversal is complete and we have found the node..then we will delete the node
+        // deleting a leaf node
         if (current->left == H && current->right == H) {
             if (parent->left == current) {
                 parent->left = H;
@@ -141,6 +152,9 @@ public:
             }
             delete current;
         }
+
+
+        //in this case the node that we have to delete has only one child node and that is the left node
         else if (current->left != H && current->right == H) {
             if (parent->left == current) {
                 parent->left = current->left;
@@ -151,6 +165,8 @@ public:
             current->left->parent = parent;
             delete current;
         }
+
+        //in this case the node that we have to delete has only one child and that is the right node 
         else if (current->left == H && current->right != H) {
             if (parent->left == current) {
                 parent->left = current->right;
@@ -161,6 +177,8 @@ public:
             current->right->parent = parent;
             delete current;
         }
+
+        //in this case the node that we have to delete has two children
         else {
             tNode<K, V>* successor = current->right;
             tNode<K, V>* successor_parent = current;
@@ -183,6 +201,8 @@ public:
         }
         n--;
     }
+
+
 
     class iterator {
         tNode<K, V>* ptr;
@@ -223,7 +243,55 @@ public:
         pair<K, V> operator*() const {
             return ptr->data;
         }
+
+        iterator& operator--() {
+            tNode<K>* current = ptr;
+
+            if (current->left->is_Head != true) {
+                current = current->left;
+                while (current->right->is_Head != true) {
+                    current = current->right;                           //pre decrement
+                }
+                ptr = current;
+            }
+            else {
+                tNode<K>* parent = current->parent;
+                while (current == parent->left && parent->is_Head != true) {
+                    current = parent;
+                    parent = parent->parent;
+                }
+                ptr = parent;
+            }
+            return *this;
+        }
+
+        iterator operator--(int) {
+            iterator temp = *this;                                  //post decrement
+            --(*this);
+            return temp;
+        }
     };
+
+    //this will be called inside the clear function
+
+    void clear_r(tNode<K, V>* current) {
+
+
+        if (current == H) {
+            return;
+        }
+        clear_r(current->left);
+        clear_r(current->right);
+        delete current;
+    }
+
+    void clear() {
+        clear_r(H->parent);
+        H->parent = H;
+        H->left = H;
+        H->right = H;
+        n = 0;
+    }
 
     iterator begin() const {
         tNode<K, V>* current = H->parent;
@@ -240,50 +308,160 @@ public:
         return iterator(H);
     }
 
-    ~map() {
-        while (H->parent != H) {
-            erase(H->parent->data.first);
+
+    iterator rbegin() const {
+        tNode<K, V>* current = H->parent;
+        if (current == H) {
+            return iterator(H);
+        }                                                       //iterator reverse begin
+        while (current->right != H) {
+            current = current->right;
         }
-        delete H;
-    }
-};
-
-int main() {
-    map<int, std::string> m;
-
-    // Insert elements
-    m.insert(10, "ten");
-    m.insert(5, "five");
-    m.insert(15, "fifteen");
-    m.insert(3, "three");
-    m.insert(7, "seven");
-
-    // Print all elements using iterator
-    std::cout << "Elements in the map: \n";
-    for (map<int, std::string>::iterator it = m.begin(); it != m.end(); ++it) {
-        pair<int, std::string> p = *it;
-        std::cout << p.first << ": " << p.second << "\n";
+        return iterator(current);
     }
 
-    // Find an element
-    std::string value;
-    if (m.find(7)) {
-        std::cout << "Found key 7 with value: " << value << std::endl;
-    }
-    else {
-        std::cout << "Key 7 not found" << std::endl;
+
+    iterator rend() const {                                     //iterator reverse end
+        iterator it;
+        it.ptr = this->H;
+        return it;
     }
 
-    // Erase an element
-    std::cout << "Erasing key 5...\n";
-    m.erase(5);
+    //making the iterator function and setting its return type as iterator
+    //placing it after the iterator implementation as it utlilized the iterator class
 
-    // Print all elements again
-    std::cout << "Elements in the map after erasing key 5: \n";
-    for (map<int, std::string>::iterator it = m.begin(); it != m.end(); ++it) {
-        pair<int, std::string> p = *it;
-        std::cout << p.first << ": " << p.second << "\n";
+    iterator find(const K& key) const {
+        tNode<K, V>* current = H->parent;
+        while (current != H) {
+            if (key == current->data.first) {
+                return iterator(current);
+            }
+            else if (key < current->data.first) {
+                current = current->left;
+            }
+            else {
+                current = current->right;
+            }
+        }
+        return iterator(H); //this acts as end() function
     }
 
-    return 0;
-}
+    //this throws an exception when the key is not found
+    V& at(const K& key) {
+        tNode<K, V>* current = H->parent;
+        while (current != H) {
+            if (key == current->data.first) {
+                return current->data.second;
+
+            }
+
+            else if (key < current->data.first) {
+                current = current->left;
+
+            }
+
+            else {
+                current = current->right;
+            }
+        }
+
+        throw std::out_of_range("Key not found");
+    }
+
+    //this inserts a default values when the key isnt found
+    V& operator[](const K& key) {
+        iterator it = find(key);
+        if (it != end()) {
+            return (*it).second;
+        }
+        else {
+            V value;
+            insert(key, value);
+            return find(key).ptr->data.second;
+        }
+    }
+
+
+    int count(const K& key)const {
+        tNode<K, V>* current = H->parent;
+
+        while (current != H) {
+
+            if (key == current->data.first) {
+
+                return -1;                                     //this function is used to check if the key is present or not in the map
+
+            }
+
+            else if (key < current->data.firt) {
+                current = current->left;
+            }
+
+            else {
+                current = current->right;
+            }
+        }
+    }
+
+    int size()const {
+        reutrn n;
+    }
+
+
+    bool empty()const {
+        return n == 0;
+    }
+
+
+    //the contain function is used to check if the key is present in the map or not..it also returns bool value
+
+    bool contains(const K& key) const {
+        tNode<K, V>* current = H->parent;
+
+        while (current != H) {
+            if (key == current->data.first) {
+                return true;
+            }
+            else if (key < current->data.first) {
+                current = current->left;
+            }
+            else {
+                current = current->right;
+            }
+        }
+    }
+
+
+    map& operator=(const map& other) {
+        if (this == &other) {
+            return *this;
+        }
+
+        clear();            //the current map is cleared....making it ready to store new vals
+
+        for (auto it = other.begin(); it != other.end(); ++it) {
+            insert(it->first, it->second);
+        }
+    }
+
+
+
+        ~map() {
+            /* while (H->parent != H) {
+           erase(H->parent->data.first);
+                 } */
+            clear();
+            delete H;
+
+
+        }
+
+
+
+
+    };
+
+
+
+
+
